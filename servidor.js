15423dae7125e7a7202e7c8cb706e299bc505265
Cristofer [Quad] <cristofer.dev@gmail.com>
2016-05-11 22:39:39 -0300

    Encapsula Servidor
    Crea una function servidor que permite encapsular
    para ser instanciado desde otros archivos.
    
    Ficher inicio.js hace un require al modulo que
    contiene la funcion servidor y luego llama a
    la funcion.

--- servidor.js
+++ servidor.js
@@ -1,9 +1,13 @@
 var servidor = require('http');
 
-function enciendeServidor(entrada,salida){
-	salida.writeHead(200,{"Content-Type":"text/html"});
-	salida.write("<h1>Servidor Encendido... \o/</h1>")
-	salida.end();
+function iniciar(){
+	function enciendeServidor(entrada,salida){
+		console.log("Conección establecida");
+		salida.writeHead(200,{"Content-Type":"text/html"});
+		salida.write("<h1>Servidor Encendido... \o/</h1>")
+		salida.end();
+	}
+	servidor.createServer(enciendeServidor).listen(8888);
 }
 
-servidor.createServer(enciendeServidor).listen(8888);
+exports.iniciar = iniciar;
