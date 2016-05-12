15b5f3afd49b55bf5e9615ef4e542fc28c22fc79
Cristofer <Cristofer.dev@gmail.com>
2016-05-12 13:42:04 -0300

    Habilitando log de peticiones
    	modified:   servidor.js

--- servidor.js
+++ servidor.js
@@ -1,6 +1,6 @@
 var servidor = require('http');
 var url = require('url');
-
+var fs = require('fs');
 
 function iniciar(enrutar,manejador){
 	function enciendeServidor(entrada,salida){
@@ -10,7 +10,10 @@ function iniciar(enrutar,manejador){
 		console.log("Conección establecida");
 
 		var contenido = enrutar(manejador,ruta);
-	
+
+		var registro = fs.createWriteStream('log.txt',{'flags':'a'});	
+		registro.write(ruta + '\n');
+
 		salida.writeHead(200,{"Content-Type":"text/html"});
 		salida.write(contenido);
 		salida.end();
