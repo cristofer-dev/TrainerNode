f21b7ef225c63f251ffa7ee0f7f84a9890e13ac5
Cristofer <Cristofer.dev@gmail.com>
2016-05-12 13:11:24 -0300

    Manejador de Peticiones inexistentes
    Se incluye estructura de control if else para validar
    que solo se ejecute una funcion existente
    
    	modified:   controlador.js

--- controlador.js
+++ controlador.js
@@ -1,6 +1,11 @@
 function enrutar(manejador,ruta){
 	console.log("Accediendo a "+ruta);
-	return manejador[ruta]();
+
+	if(typeof manejador[ruta]==='function'){
+		return manejador[ruta]();
+	}else{
+		console.log("No Existe una funcion para esa ruta");
+	}
 }
 
 exports.enrutar = enrutar;
